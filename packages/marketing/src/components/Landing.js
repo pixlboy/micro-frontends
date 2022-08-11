20e3dfe0bee6a6cff72d24b1575650cb80b1ffe0
Rachit Garg <rachigarg@gmail.com>
2022-08-11 20:30:00 +0530

    test workflow marketing

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -96,7 +96,7 @@ export default function Album() {
                 <Grid item>
                   <Link to="/pricing">
                     <Button variant="contained" color="primary">
-                      Pricing
+                      Pricings
                     </Button>
                   </Link>
                 </Grid>
